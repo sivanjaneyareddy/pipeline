4b441bbda375172eee0b4953b38701ef112f17fb
Pranay Shavili <Pranay.Shavili@realogy.com>
2021-10-03 19:18:34 +0000

    Updated MySQLVM.sh

--- Scripts/MySQLVM.sh
+++ Scripts/MySQLVM.sh
@@ -5,16 +5,14 @@ sudo yum install epel-release -y
 sudo yum install git zip unzip -y
 sudo yum install mariadb-server -y
 
- 
-
 
 # starting & enabling mariadb-server
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
-git clone -b local-setup https://github.com/Pranay-26/vprofile-project.git
+git clone -b vp-rem https://github.com/devopshydclub/vprofile-repo.git
 #restore the dump file for the application
-sudo mysql -u root password "$DATABASE_PASS"
+sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
@@ -23,15 +21,11 @@ sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 sudo mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123'"
-sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-project/src/main/resources/db_backup.sql
+sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-repo/src/main/resources/db_backup.sql
 sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
-
- 
 
 # Restart mariadb-server
 sudo systemctl restart mariadb
-
- 
 
 
 #starting the firewall and allowing the mariadb to access from port no. 3306
